ed6de2d77cbd2e9bcdc11209c118ad1840757f0d
yash-levi1896 <yashkumar18gupta@gmail.com>
2023-05-09 18:43:42 +0530

    routes

--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -1,12 +1,12 @@
 const express=require("express")
 
-const {UserModel}=require("../models/User.model")
+const {UserModel}=require("../Models/User.model")
 const userRouter=express.Router()
 const jwt=require("jsonwebtoken")
 const bcrypt=require("bcrypt")
-const {blacklist}=require("../models/blacklist")
+const {blacklist}=require("../Models/blacklist")
 const {authenticate}=require("../middlewares/authenticate.middleware")
-const { adminmodel } = require("../models/admin.model")
+const { adminmodel } = require("../Models/admin.model")
 
 const {passport} = require("../google_auth")
 // const {client} = require("../middlewares/redis")
